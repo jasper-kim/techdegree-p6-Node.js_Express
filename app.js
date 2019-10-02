bd9d6cc36daa273a870f825c1b02dc222952528e
Jasper Kim <yjikim0219@gmail.com>
2019-10-02 11:29:29 -0700

    Updated Error handling

--- app.js
+++ app.js
@@ -23,28 +23,33 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/project/:id', (req, res) => {
-    res.render('project', {
-        project_name: projects[req.params.id].project_name,
-        description: projects[req.params.id].description,
-        technologies: projects[req.params.id].technologies,
-        live_link: projects[req.params.id].live_link,
-        github_link: projects[req.params.id].github_link,
-        image_urls: projects[req.params.id].image_urls
-    });
+app.get('/project/:id', (req, res, next) => {
+    if (req.params.id < projects.length) {
+        res.render('project', {
+            project_name: projects[req.params.id].project_name,
+            description: projects[req.params.id].description,
+            technologies: projects[req.params.id].technologies,
+            live_link: projects[req.params.id].live_link,
+            github_link: projects[req.params.id].github_link,
+            image_urls: projects[req.params.id].image_urls
+        });
+    } else {
+        next();
+    }
+    
 });
 
 //Set Error object
 app.use((req, res, next) => {
-    // const err = new Error("Page Not Found :(");
-    // err.status = 404;
-    // next(err);
+    const err = new Error("Page Not Found :(");
+    err.status = 404;
+    next(err);
 
-    if(!req.route){
-        console.log('Wrong path!');
-    } else {
-        console.log('Right path!');
-    }
+    // if(!req.route){
+    //     console.log('Wrong path!');
+    // } else {
+    //     console.log('Right path!');
+    // }
 });
 
 //Set error error handling middleware
